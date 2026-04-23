1dfe33b2bf5c595a0930a46c4140e12857d418a5
unknown <starki.gregoire2007@gmail.com>
2026-04-23 13:10:46 +0200

    les modification sur le main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
-    print("Bienvenue dans le gestionnaire de tâches")
+    print("Bienvenue dans le gestionnaire de tâches  et dans le monde 
+
+")
 
 if __name__ == "__main__":
     main()
